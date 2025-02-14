fa4497130d9038242a04e5e1cf0eb5ea7ff2786c
[キョウコ-T] <[balalaika78koko@gmail.com]>
2025-02-14 02:40:13 +0000

    Laravel 13 テキスト学習

--- mynews/app/Http/Controllers/Admin/NewsController.php
+++ mynews/app/Http/Controllers/Admin/NewsController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
     public function add()
     {
         return view(' admin.news.create');
+    }
+
+    //Laravel 13 ニュース投稿画面を作成しようで追記
+    public function create(Request $request)
+    {
+        //admin/news/createにリダイレクトする
+        return redirect('admin/news/create');
     }
 }
