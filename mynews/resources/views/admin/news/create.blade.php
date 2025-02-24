7d5ce3008bc94bfc9a6a3314d66d36c94fa6ccec
[キョウコ-T] <[balalaika78koko@gmail.com]>
2025-02-24 07:35:16 +0000

    Laravel13 課題4・5

--- mynews/resources/views/admin/news/create.blade.php
+++ mynews/resources/views/admin/news/create.blade.php
@@ -13,20 +13,20 @@
                 <h2>ニュース新規作成</h2>
                 <form action="{{ route('admin.news.create') }}" method="post" enctype="multipart/form-data">
 
-                @if (count($errors) > 0)
-                    <ul>
-                        @foreach($errors->all() as $e)
-                        <li>{{ $e }}</li>
-                        @endforeach
-                    </ul>
-                @endif
-                <div class="form-group row">
-                    <label class="col-md-2">タイトル</label>
-                    <div class="col-md-10">
-                        <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">タイトル</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
+                    <div class="form-group row">
                         <label class="col-md-2">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
