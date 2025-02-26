2cfeb6e0963ac84900b9f8b2aa0d38e39e01a12e
[キョウコ-T] <[balalaika78koko@gmail.com]>
2025-02-26 07:20:21 +0000

    Laravel14 News Contorollerを編集

--- mynews/app/Http/Controllers/Admin/NewsController.php
+++ mynews/app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 以下の１行を追加することでNews Modelが扱えるようになる
+use App\Models\News;
+
 class NewsController extends Controller
 {
     public function add()
@@ -12,10 +15,34 @@ class NewsController extends Controller
         return view(' admin.news.create');
     }
 
-    //Laravel 13 ニュース投稿画面を作成しようで追記
+    // Laravel 13 ニュース投稿画面を作成しようで追記
     public function create(Request $request)
     {
-        //admin/news/createにリダイレクトする
+        // Laravel14 NewsController の操作で追記
+        // validationを行う
+        $this->validate($request, News::$rules);
+
+        $news = new News;
+        $form = $request->all();
+
+        //  フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+        if (isset($form['image'])){
+            $path = $request->file('image')->store('public/image');
+            $news ->image_path = basename($path);
+        } else {
+            $news-> image_path = null;
+        }
+
+        //  フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']); 
+        
+        // データベースに保存する
+        $news->fill($form);
+        $news->save();
+
+        // admin/news/createにリダイレクトする
         return redirect('admin/news/create');
     }
 }
