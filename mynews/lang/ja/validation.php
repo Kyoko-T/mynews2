4f35b711abb8e11a8834d24ebf1840cc2dc269bf
[キョウコ-T] <[balalaika78koko@gmail.com]>
2025-03-01 08:09:19 +0000

    Laravel14課題6関連

--- mynews/lang/ja/validation.php
+++ mynews/lang/ja/validation.php
@@ -168,7 +168,11 @@ return [
     'attributes' => [
         //以下Laravel14で追記
         'title' => 'タイトル',
-        'body' => '本文'
+        'body' => '本文',
+        'name' => '名前',
+        'gender' => '性別',
+        'hobby' => '趣味',
+        'introduction' => '自己紹介'
     ],
 
 ];
