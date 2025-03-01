e03aa453a001117e713cd39b1ea483fd30dc1d75
[キョウコ-T] <[balalaika78koko@gmail.com]>
2025-03-01 08:01:57 +0000

    Laravel14課題5

---
+++ mynews/app/Models/Profile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    //Laravel14課題5で追記
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+}
