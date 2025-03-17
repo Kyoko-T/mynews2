803db6d5914c7e45719fb288486f15b881329ec0
[キョウコ-T] <[balalaika78koko@gmail.com]>
2025-03-17 04:48:49 +0000

    Laravel17 テーブル同士の関連

--- mynews/app/Models/News.php
+++ mynews/app/Models/News.php
@@ -8,11 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
-    //以下Laravel14 Validationで追記
+    // 以下Laravel14 Validationで追記
     protected $guarded = array('id');
 
     public static $rules =array(
         'title' => 'required',
         'body' => 'required',
     );
+
+    // 以下Laravel17で追記
+    // News Modelに関連付けを行う
+    public function histories()
+    {
+        return $this->hasmany('App\Models\History');
+    }
 }
